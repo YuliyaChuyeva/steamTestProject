75c332577b4a1e33451f19c5c1009b3c0e372fc4
YuliyaChuyeva <yuliachueva@icloud.com>
2025-09-03 09:13:27 +0300

    factory

--- src/main/java/core/Driver.java
+++ src/main/java/core/Driver.java
@@ -1,20 +1,34 @@
 package core;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class Driver {
     private static WebDriver driver;
+    private static final Logger logger = LoggerFactory.getLogger(Driver.class);
 
     private Driver() {
     }
 
-    public static WebDriver getDriver() {
+    public static WebDriver getInstance() {
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = DriverFactory.initDriver();
         }
         return driver;
+    }
+
+    public static void navigateMainPage() {
+        driver.manage().window().maximize();
+        String url = PropertiesReader.getInstance().getUrl();
+        driver.get(url);
+        logger.info("Переход на главную страницу: {}", url);
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
---
+++ src/main/java/core/DriverFactory.java
@@ -0,0 +1,37 @@
+package core;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class DriverFactory {
+    private static final String CHROME = "CHROME";
+    private static final String FIREFOX = "FIREFOX";
+    private static final String EDGE = "EDGE";
+
+    public static WebDriver initDriver() {
+        String browserName = PropertiesReader.getInstance().getBrowser();
+        WebDriver driver;
+        switch (browserName.toUpperCase()) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+        }
+        return driver;
+    }
+}
