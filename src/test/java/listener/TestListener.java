a7a3be746d2d3abd75f6e4f4da7d418704893d18
YuliyaChuyeva <yuliachueva@icloud.com>
2026-02-19 14:14:25 +0300

    screenshots and allure

--- src/test/java/listener/TestListener.java
+++ src/test/java/listener/TestListener.java
@@ -2,7 +2,6 @@ package listener;
 
 import core.driver.Driver;
 import core.util.PropertiesReader;
-import io.qameta.allure.Attachment;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -10,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import util.AllureAttachments;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,7 +21,7 @@ public class TestListener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         trySaveScreenshot(result);
-        attachScreenshotToAllure();
+        AllureAttachments.attachScreenshotToAllure();
     }
 
     private void trySaveScreenshot(ITestResult result) {
@@ -37,17 +37,5 @@ public class TestListener implements ITestListener {
         } catch (IOException | WebDriverException e) {
             log.warn("Failed to save screenshot for test: {}", result.getMethod().getMethodName(), e);
         }
-    }
-
-    private void attachScreenshotToAllure() {
-        WebDriver driver = Driver.getInstance();
-        if (driver == null) return;
-        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        attach(screenshot);
-    }
-
-    @Attachment(value = "Screenshot", type = "image/png")
-    private byte[] attach(byte[] screenshot) {
-        return screenshot;
     }
 }
